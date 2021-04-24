35fa0c3e9975a93aed7a55660fd0234386dc0e39
JH-Byun <quswjdgus97@gmail.com>
2021-04-24 16:26:50 +0900

    2021-04-23-16-26

--- AKF - TP1/JH_AF_TP1_2_c_2.m
+++ AKF - TP1/JH_AF_TP1_2_c_2.m
@@ -20,111 +20,113 @@ addpath('source');
 %% Simple demonstration for IMM using velocity and acceleration models
 
 % Dimensionality of the state space
-dims = 6;
+dims = 4;
 
 % Space for models
-ind = cell(1,2);
-A = cell(1,2);
-Q = cell(1,2);
-H = cell(1,2);
-R = cell(1,2);
+ind = cell(1,3);
+A = cell(1,3);
+Q = cell(1,3);
+H = cell(1,3);
+R = cell(1,3);
 
 % Stepsize
 dt = 0.1;
 
-ind{2} = [1 2 3 4 5 6]';
-
-% Transition matrix for the continous-time acceleration model.
-F2 = [0 0 1 0 0 0;
-      0 0 0 1 0 0;
-      0 0 0 0 1 0;
-      0 0 0 0 0 1;
-      0 0 0 0 0 0;
-      0 0 0 0 0 0];
-
-% Noise effect matrix for the continous-time system.
-L2 = [0 0;
-      0 0;
-      0 0;
-      0 0;
-      1 0;
-      0 1];
-
-% Process noise variance
-q2 = 1.00;
-Qc2 = diag([q2 q2]);
-
-% Discretization of the continous-time system.
-[A{2},Q{2}] = lti_disc(F2,L2,Qc2,dt);
-
-ind{1} = [1 2 3 4]';
-
 % Transition matrix for the continous-time velocity model.
-F1 = [0 0 1 0;
+F = [0 0 1 0;
       0 0 0 1;
       0 0 0 0;
       0 0 0 0];
 
 % Noise effect matrix for the continous-time system.
-L1 = [0 0;
+L = [0 0;
       0 0;
       1 0;
       0 1];
 
-  % Process noise variance
-q1 = .01;
+% model 1
+ind{1} = [1 2 3 4]';
+
+% Process noise variance 1
+q1 = 0.2^2;
 Qc1 = diag([q1 q1]);
 
-% Discretization of the continous-time system.
-[A{1},Q{1}] = lti_disc(F1,L1,Qc1,dt);
+% Discretization of the continous-time system. 1
+[A{1},Q{1}] = lti_disc(F,L,Qc1,dt);
 
+% Measurement model 1
 H{1} = [1 0 0 0;
         0 1 0 0];
 
-% Measurement models.
-H{2} = [1 0 0 0 0 0;
-        0 1 0 0 0 0];
+% model 2
+ind{2} = [1 2 3 4]';
+
+% Process noise variance 2
+q2 = 3^2;
+Qc2 = diag([q2 q2]);
+
+% Discretization of the continous-time system 2
+[A{2},Q{2}] = lti_disc(F,L,Qc2,dt);
+
+% Measurement model 2
+H{2} = [1 0 0 0;
+        0 1 0 0];
+    
+% model 3
+ind{3} = [1 2 3 4]';
+
+% Process noise variance 3
+q3 = 5^2;
+Qc3 = diag([q3 q3]);
+
+% Discretization of the continous-time system 2
+[A{3},Q{3}] = lti_disc(F,L,Qc3,dt);
+
+% Measurement model 2
+H{3} = [1 0 0 0;
+        0 1 0 0];
+    
 hdims = 2;
 
 % Variance in the measurements.
-r1 = .1;
-r2 = .1;
+r1 = 1;
+r2 = 1;
 R{1} = diag([r1 r2]);
 
-r1 = .1;
-r2 = .1;
+r1 = 1;
+r2 = 1;
 R{2} = diag([r1 r2]);
+
+r1 = 1;
+r2 = 1;
+R{3} = diag([r1 r2]);
 
 % Generate the data.
 n = 200;
 Y = zeros(hdims,n);
 X_r = zeros(dims,n);
-X_r(:,1) = [0 0 0 -1 0 0]';
+X_r(:,1) = [250 100 10 0]';
 mstate = zeros(1,n);
 
-mu_ip = [0.1 0.9];
+mu_ip{1} = [0.3 0.3 0.4];
+% (ADDED) initial distribution
+mu_ip{2} = [0.1 0.4 0.5];
+mu_ip{3} = [0.1 0.8 0.1];
+mu_ip{4} = [0.2 0.1 0.7];
+mu_ip{5} = [0.9 0.05 0.05];
 mu_0j = mu_ip;
 
 % case 1 : ideal
-p_ij{1} = [0.98 0.02;
-        0.02 0.98];
-
-% (ADDED) case 2 ~ 5    
-p_ij{2} = [0.9 0.1;...
-    0.1 0.9];
-p_ij{3} = [0.8 0.2;...
-    0.2 0.8];
-p_ij{4} = [0.75 0.25;...
-    0.25 0.75];
-p_ij{5} = [0.7 0.3;...
-    0.3 0.7];
+p_ij = [0.98 0.01 0.01;...
+    0.01 0.98 0.01;...
+    0.01 0.01 0.98];
 
 % Forced mode transitions 
-mstate(1:50) = 2;
-mstate(51:70) = 2;
-mstate(71:120) = 2;
-mstate(121:150) = 1;
-mstate(151:200) = 1;
+qstate(1:50) = 0.2;
+qstate(51:70) = 1.5;
+qstate(71:120) = 3;
+qstate(121:150) = 4;
+qstate(151:200) = 5;
 
 % !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! %
 % random seed - do not change      %
@@ -133,30 +135,28 @@ rng(15);
 
 % Acceleration model
 for i = 2:n
-    st = mstate(i);
-    X_r(ind{st},i) = A{st}*X_r(ind{st},i-1) + gauss_rnd(zeros(size(A{st},1),1), Q{st});
+    [A_temp,Q_temp] = lti_disc(F,L,diag([qstate(i)^2 qstate(i)^2]),dt);
+    X_r(ind{1},i) = A{1}*X_r(ind{1},i-1) + gauss_rnd(zeros(size(A{1},1),1), Q_temp);
 end
 
 % Generate the measurements.
 for i = 1:n
-    Y(:,i) = H{mstate(i)}*X_r(ind{mstate(i)},i) + gauss_rnd(zeros(size(Y,1),1), R{mstate(i)});
+    Y(:,i) = H{1}*X_r(ind{1},i) + gauss_rnd(zeros(size(Y,1),1), R{1});
 end
-
 
 %% main algorithm
 
-m = [0 0 0 -1 0 0]';
-P = diag([0.1^2 0.1^2 0.1^2 0.1^2 0.5^2 0.5^2]);
+m = [250 100 10 0]';
+P = diag([5 5 1 1]);
 
 %%%% Space for the estimates %%%% 
 
 % Overall estimates of IMM filter
 MM = cell(5,n);
 PP = cell(5,n);
-
 % Model-conditioned estimates of IMM
-MM_i = cell(2,n);
-PP_i = cell(2,n);
+MM_i = cell(3,n);
+PP_i = cell(3,n);
 
 % Model probabilities 
 MU = cell(5,n);
@@ -164,46 +164,45 @@ MU = cell(5,n);
 %%%% Prior estimates %%%%
 
 % IMM
-x_ip{1} = [0 0 0 -1]';
-P_ip{1} = diag([0.1^2 0.1^2 0.1^2 0.1^2]);
-x_ip{2} = [0 0 0 -1 0 0]';
-P_ip{2} = diag([0.1^2 0.1^2 0.1^2 0.1^2 0.5^2 0.5^2]);
+x_ip = [250 100 10 0]';
+P_ip = diag([5 5 1 1]);
 
 % (ADDED) IMM initialization
 for k = 1:5
-    MU{k} = mu_ip.';
+    MU{k,1} = mu_ip.';
 end
 
 % Filtering steps.
 for k = 1:5
-    % (ADDED) IMM initialization
-    MM_i{1,1} = x_ip{1};
-    MM_i{2,1} = x_ip{2};
-    PP_i{1,1} = P_ip{1};
-    PP_i{2,1} = P_ip{2};
-    c_j = mu_0j.';
+    MM_i{1,1} = x_ip;
+    MM_i{2,1} = x_ip;
+    MM_i{3,1} = x_ip;
+    PP_i{1,1} = P_ip;
+    PP_i{2,1} = P_ip;
+    PP_i{3,1} = P_ip;
+    c_j = mu_0j{k}.';
     for i = 1:size(Y,2)
-        % case k        
+        % IMM - 
         if i ~= 1
-            MM_temp{1} = MM_i{1,i-1};  MM_temp{2} = MM_i{2,i-1};
-            PP_temp{1} = PP_i{1,i-1}; PP_temp{2} = PP_i{2,i-1};
-            [MM_temp, PP_temp, c_j, MM{k,i}, PP{k,i}] = imm_predict(MM_temp, PP_temp, MU{k,i-1}, p_ij{k}, ind, dims, A, Q); % (ADDED) IMM prediction
-            MM_i{1,i} = MM_temp{1}; MM_i{2,i} = MM_temp{2};
-            PP_i{1,i} = PP_temp{1}; PP_i{2,i} = PP_temp{2};
+            MM_temp{1} = MM_i{1,i-1}; MM_temp{2} = MM_i{2,i-1}; MM_temp{3} = MM_i{3,i-1};
+            PP_temp{1} = PP_i{1,i-1}; PP_temp{2} = PP_i{2,i-1}; PP_temp{3} = PP_i{3,i-1};
+            [MM_temp, PP_temp, c_j, MM{k,i}, PP{k,i}] = imm_predict(MM_temp, PP_temp, MU{k,i-1}, p_ij, ind, dims, A, Q); % (ADDED) IMM prediction
+            MM_i{1,i} = MM_temp{1}; MM_i{2,i} = MM_temp{2}; MM_i{3,i} = MM_temp{3};
+            PP_i{1,i} = PP_temp{1}; PP_i{2,i} = PP_temp{2}; PP_i{3,i} = PP_temp{3};
         end
-        MM_temp{1} = MM_i{1,i};  MM_temp{2} = MM_i{2,i};
-        PP_temp{1} = PP_i{1,i}; PP_temp{2} = PP_i{2,i};
+        MM_temp{1} = MM_i{1,i};  MM_temp{2} = MM_i{2,i}; MM_temp{3} = MM_i{3,i};
+        PP_temp{1} = PP_i{1,i}; PP_temp{2} = PP_i{2,i}; PP_temp{3} = PP_i{3,i};
         [MM_temp, PP_temp, MU{k,i}, MM{k,i}, PP{k,i}] = imm_update(MM_temp, PP_temp, c_j, ind, dims, Y(:,i), H, R); % (ADDED) IMM update
-        MM_i{1,i} = MM_temp{1}; MM_i{2,i} = MM_temp{2};
-        PP_i{1,i} = PP_temp{1}; PP_i{2,i} = PP_temp{2};
+        MM_i{1,i} = MM_temp{1}; MM_i{2,i} = MM_temp{2}; MM_i{3,i} = MM_temp{3};
+        PP_i{1,i} = PP_temp{1}; PP_i{2,i} = PP_temp{2}; PP_i{3,i} = PP_temp{3};
     end
 end
 
 %% Calculate the MSEs
 % (ADDED)
-sum = cell(5,1);
+sum{k} = cell(5,1);
 for k = 1:5
-    sum{k} = zeros(6,1);
+    sum{k} = zeros(4,1);
 end
 % (ADDED) get summation of the squares of the estimation errors from each
 % time step
@@ -226,7 +225,7 @@ fprintf('MSE(IMM_case 5) - x: %f y: %f v_x: %f v_y: %f \n', MSE{5}(1), MSE{5}(2)
 %% (ADDED) Plot estimation result
 tspan = dt*(1:n); % (ADDED) time span
 err_arr = cell(5,1);
-MU_arr = cell(2,1);
+MU_arr = cell(3,1);
 for k = 1:5
     for i = 1:size(Y,2)
         err_arr{k}(:,i) = X_r(:,i) - MM{k,i};
@@ -295,7 +294,7 @@ title('Velocity y error');
 
 %% (ADDED) Model Probability
 figure;
-subplot(2,1,1)
+subplot(3,1,1)
 plot(tspan, MU_arr{1}(1,:), 'LineWidth', 2)
 hold on
 plot(tspan, MU_arr{2}(1,:), 'LineWidth', 2)
@@ -310,7 +309,7 @@ ylabel('\mu_1 [-]');
 ylim([0 1]);
 legend('case 1','case 2', 'case 3', 'case 4', 'case 5');
 title('model 1 probability plot');
-subplot(2,1,2)
+subplot(3,1,2)
 plot(tspan, MU_arr{1}(2,:), 'LineWidth', 2)
 hold on
 plot(tspan, MU_arr{2}(2,:), 'LineWidth', 2)
@@ -321,6 +320,22 @@ plot(tspan, MU_arr{4}(2,:), 'LineWidth', 2)
 hold on
 plot(tspan, MU_arr{5}(2,:), 'LineWidth', 2)
 grid on
+ylabel('\mu_2 [-]');
+ylim([0 1]);
+legend('case 1','case 2', 'case 3', 'case 4', 'case 5');
+title('model 2 probability plot');
+subplot(3,1,3)
+plot(tspan, MU_arr{1}(2,:), 'LineWidth', 2)
+hold on
+plot(tspan, MU_arr{2}(2,:), 'LineWidth', 2)
+hold on
+plot(tspan, MU_arr{3}(2,:), 'LineWidth', 2)
+hold on
+plot(tspan, MU_arr{4}(2,:), 'LineWidth', 2)
+hold on
+plot(tspan, MU_arr{5}(2,:), 'LineWidth', 2)
+grid on
+xlabel('time [sec]');
 ylabel('\mu_2 [-]');
 ylim([0 1]);
 legend('case 1','case 2', 'case 3', 'case 4', 'case 5');
